238e8e5845266e97eb4db225ea2321c841cf7878
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-02 15:20:49 +0000

    use sass compiler

--- src/main.js
+++ src/main.js
@@ -1,10 +1,1 @@
-// import notification from './Notification';
-// 
-// notification.notify('hello once again');
-// notification.log('in the log again');
-
-// require('./main.css');
-
-import {Form} from './Form';
-
-new Form;
+require('./main.scss');
